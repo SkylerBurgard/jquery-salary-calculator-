80fb228fc0b215a6be914354b0b95780aba732cb
Skyler Burgard <skylerrburgard@hotmail.com>
2020-04-02 18:31:23 -0500

    Fixed server starterror using back ticks, removing a bracket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const PORT = 5000;
 
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/math', (req, res)=> {
+app.get('/math', (req, res) => {
 
 });
 
@@ -15,8 +15,8 @@ app.post('/math', (req, res) => {
 
 });
 
-app.listen({PORT, () => {
-console.log('Listening on ports: ${PORT}');
+app.listen(PORT, () => {
+    console.log(`Listening on ports: ${PORT}`);
 })
 
 // Having trouble trouble shooting the terminal start up.
